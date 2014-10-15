a59f2b0b0417f20c6dc2d2cc839f92f71db4489f
jamesascarter <jamesascarter@gmail.com>
2014-10-15 19:31:09 +0100

    Added game rules for RP and S

--- spec/RpsSpec.js
+++ spec/RpsSpec.js
@@ -24,5 +24,30 @@ describe("RockPaperScissors", function(){
 
 	});
 
+	describe("rules of the game", function() {
+
+		rules = new Rules
+
+		it("When same gestures drawn it is a draw", function() {
+			expect(rules.versus("Rock", "Rock")).toEqual("Draw");
+		});
+
+		it("Rock beats scissors", function(){
+			expect(rules.versus("Rock", "Scissors")).toEqual("Rock wins");
+
+		});
+
+		it("Paper beats rock", function(){
+			expect(rules.versus("Paper", "Rock")).toEqual("Paper wins")
+		});
+
+		it("Scissors beats paper", function(){
+			expect(rules.versus("Scissors", "Paper")).toEqual("Scissors wins")
+		});
+
+
+
+	});
+
 
 });
--- src/Rps.js
+++ src/Rps.js
@@ -1,4 +1,5 @@
 function Rules() {};
+
 function Rock() {
 	this.type = "Rock";
 };
@@ -7,4 +8,15 @@ function Paper() {
 };
 function Scissors(){
 	this.type = "Scissors"
+};
+
+Rules.prototype.versus = function(choice1, choice2){
+	if(choice1 === choice2)
+		return "Draw";
+	else if (choice1 === "Rock" && choice2 === "Scissors")
+		return "Rock wins";
+	else if (choice1 === "Paper" && choice2 === "Rock")
+		return "Paper wins"
+	else (choice1 === "Scissors" && choice2 === "Paper")
+		return "Scissors wins" 
 };
